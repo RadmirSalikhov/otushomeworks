d5d323f9cf529388aec5e17459089675937a4fd4
Radmir <radmir.sf.02@gmail.com>
2022-11-01 21:00:21 +0300

    HomeWork3

---
+++ src/ru/otus/homeworks/hw02/HomeworkTemplate.java
@@ -0,0 +1,41 @@
+package ru.otus.homeworks.hw02;
+
+import java.util.Scanner;
+
+public class HomeworkTemplate {
+
+    public static void main(String[] args) {
+
+              System.out.println("Начнем тестирование №1. Для выхода наберите 0");
+        System.out.println("Для ответа впишите соответсвующую цифру: ");
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            String questionFirst = "Сколько будет 1+10? \n1. 10 \n2. 11 \n3. 12";
+            String questionSecond = "\nКакого цвета небо? \n1. Зеленое \n2. Голубое \n3. Желтое";
+            String questionThird = "\nЧто следует за Зимой \n1. Снег \n2. Лето \n3. Весна";
+
+            String[][] matrix = {
+                    {questionFirst, "A", "B", "A"},
+                    {questionSecond, "A", "B", "A"},
+                    {questionThird, "A", "A", "B"}};
+
+            for (int i = 0; i < matrix.length; i++) {
+                System.out.println(matrix[i][0]);
+                String answer = scanner.nextLine();
+
+                if (answer.toLowerCase().trim().equals("0")) {
+                    System.out.println("До свидания");
+                    break;
+                }
+                else if ((matrix[i][Integer.parseInt(answer)]).equals("B")) {
+                    System.out.println("Правильно");
+
+                }
+                else if ((matrix[i][Integer.parseInt(answer)]).equals("A")) {
+                    System.out.println("Не правильно");
+
+                }
+            }
+        }
+    }
+}
